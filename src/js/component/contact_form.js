d0198ae1249c285844b1961aaddeee60416ab16e
John Rohan <john.rohan87@gmail.com>
2020-12-22 23:03:30 +0000

    form update

--- src/js/component/contact_form.js
+++ src/js/component/contact_form.js
@@ -22,49 +22,45 @@ const encode = data => {
 		.join("&");
 };
 
-export default class ContactForm extends React.Component {
+class ContactForm extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = { name: "", email: "", message: "" };
 	}
 
-	handleSubmit = e => {
-		//e.preventDefault();
-		var oForm = document.forms["contact"];
-		console.log(oForm);
+	/* Here’s the juicy bit for posting the form submission */
 
+	handleSubmit = e => {
 		fetch("/", {
 			method: "POST",
 			headers: { "Content-Type": "application/x-www-form-urlencoded" },
 			body: encode({ "form-name": "contact", ...this.state })
 		})
-			.then(e => alert("Success!", e))
+			.then(() => alert("Success!"))
 			.catch(error => alert(error));
 
 		e.preventDefault();
 	};
 
+	handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
 	render() {
+		const { name, email, message } = this.state;
 		return (
-			<form name="contact" method="post" data-netlify="true" onSubmit={this.handleSubmit}>
-				<p hidden>
+			<form onSubmit={this.handleSubmit}>
+				<p>
 					<label>
-						Don’t fill this out: <input name="bot-field" />
+						Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
 					</label>
 				</p>
 				<p>
 					<label>
-						Your Name: <input type="text" name="name" />
+						Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
 					</label>
 				</p>
 				<p>
 					<label>
-						Your Email: <input type="email" name="email" />
-					</label>
-				</p>
-				<p>
-					<label>
-						Message: <textarea name="message" />
+						Message: <textarea name="message" value={message} onChange={this.handleChange} />
 					</label>
 				</p>
 				<p>
