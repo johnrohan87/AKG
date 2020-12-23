ddf5664364d53b7090000515cf4804b2366fe9aa
John Rohan <john.rohan87@gmail.com>
2020-12-23 00:57:31 +0000

    debug

--- src/js/component/contact_form.js
+++ src/js/component/contact_form.js
@@ -70,17 +70,7 @@ class ContactForm extends React.Component {
 	render() {
 		const { name, email, phone, address, message } = this.state;
 		return (
-			<form
-				netlify-honeypot="bot-field"
-				data-netlify="true"
-				data-netlify-recaptcha="true"
-				onSubmit={this.handleSubmit}>
-				<p className="hidden" hidden="true">
-					<label>
-						Don’t fill this out if you’re human: <input name="bot-field" />
-					</label>
-				</p>
-
+			<form data-netlify="true" data-netlify-recaptcha="true" onSubmit={this.handleSubmit}>
 				<div className="container-fluid p-0">
 					<div className="bg-primary p-3 border border-light">
 						<img src={FreeQuote} className="w-100" />
