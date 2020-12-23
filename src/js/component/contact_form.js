c64f62bd6c75b820fcad1c1d69849ea7cbfac09c
John Rohan <john.rohan87@gmail.com>
2020-12-23 00:59:41 +0000

    debug

--- src/js/component/contact_form.js
+++ src/js/component/contact_form.js
@@ -70,7 +70,7 @@ class ContactForm extends React.Component {
 	render() {
 		const { name, email, phone, address, message } = this.state;
 		return (
-			<form data-netlify="true" data-netlify-recaptcha="true" onSubmit={this.handleSubmit}>
+			<form netlify data-netlify-recaptcha="true" onSubmit={this.handleSubmit}>
 				<div className="container-fluid p-0">
 					<div className="bg-primary p-3 border border-light">
 						<img src={FreeQuote} className="w-100" />
